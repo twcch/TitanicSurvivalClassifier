e21d251e50ea6bf54d29bae778726f4bbf793442
Alan Hsieh <twcch1218@gmail.com>
2025-11-23 21:40:15 +0800

    feat: implement MLPipeline class with training and inference methods

--- core/pipeline/ml_pipeline.py
+++ core/pipeline/ml_pipeline.py
@@ -1,12 +1,55 @@
+from core.data.data_loader import DataLoader
+from core.preprocessing.preprocessor import (
+    PreprocessingPipeline,
+    MissingValueHandler,
+    DropColumnsPreprocessor,
+)
+from core.features.feature_engineer import (
+    FeatureEngineerPipeline,
+    OneHotEncoder,
+)
+from core.models.decision_tree_classifier_model import DecisionTreeClassifierModel
 
 
 class MLPipeline:
     def __init__(self):
-        pass
+        self.data_loader = DataLoader()
 
-    def run_training_pipeline(self, data):
-        pass
-    
-    def run_inference_pipeline(self, model, data):
-        pass
+        preprocessing_steps = [
+            MissingValueHandler(strategy="mean"),
+            DropColumnsPreprocessor(columns_to_drop=["Cabin"]),
+        ]
 
+        self.preprocessing_pipeline = PreprocessingPipeline(steps=preprocessing_steps)
+
+        feature_engineering_steps = [
+            OneHotEncoder(columns=["Sex", "Embarked", "Pclass"]),
+        ]
+
+        self.feature_engineer_pipeline = FeatureEngineerPipeline(
+            steps=feature_engineering_steps
+        )
+
+        self.model = DecisionTreeClassifierModel()
+
+    def run_training_pipeline(self, train_path: str):
+        df = self.data_loader.load_data(train_path)
+
+        y = df["Survived"]
+        X = df.drop(columns=["Survived"])
+
+        X = self.preprocessing_pipeline.fit_transform(X)
+        X = self.feature_engineer_pipeline.fit_transform(X)
+
+        self.model.train(X, y)
+        self.model.save_model("decision_tree_model.pkl")
+
+    def run_inference_pipeline(self, model_path: str, test_path: str):
+        df = self.data_loader.load_data(test_path)
+        df = self.preprocessing_pipeline.transform(df)
+        df = self.feature_engineer_pipeline.transform(df)
+
+        self.model.load_model(model_path)
+        predictions = self.model.predict(df)
+
+        print(predictions)
