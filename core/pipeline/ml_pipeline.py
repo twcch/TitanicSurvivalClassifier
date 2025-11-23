4081dce1d550ccc39b594592ab5f2322e84fe83e
Alan Hsieh <twcch1218@gmail.com>
2025-11-23 19:30:32 +0800

    feat: add MLPipeline class with methods for training and inference pipelines

---
+++ core/pipeline/ml_pipeline.py
@@ -0,0 +1,12 @@
+
+
+class MLPipeline:
+    def __init__(self):
+        pass
+
+    def run_training_pipeline(self, data):
+        pass
+    
+    def run_inference_pipeline(self, model, data):
+        pass
+
