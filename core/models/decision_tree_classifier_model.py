a9846bbc9c77cce12e2420cf9f56dffab47835b2
Alan Hsieh <twcch1218@gmail.com>
2025-11-24 00:14:07 +0800

    fix: adjust min_samples_leaf in DecisionTreeClassifierModel and update submission predictions

--- core/models/decision_tree_classifier_model.py
+++ core/models/decision_tree_classifier_model.py
@@ -16,7 +16,7 @@ class DecisionTreeClassifierModel(BaseModel):
             criterion="gini",
             max_depth=5,
             min_samples_split=2,
-            min_samples_leaf=1,
+            min_samples_leaf=10,
             random_state=42,
         )
 
